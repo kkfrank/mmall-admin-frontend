d122481c006ebf4cccdb5e82cd323619a40eb470
fankaikai <fankaikai@unity3d.com>
2020-03-22 00:01:06 +0800

    update webpack and react version

--- webpack.config.js
+++ webpack.config.js
@@ -1,107 +1,82 @@
-/*global
-    __DEV__
-    __dirname
-    process
-*/
-const webpack = require('webpack');
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+    mode: 'development',
     devtool: 'cheap-module-eval-source-map',
-    entry: [
-        path.join(__dirname, './src/index.js'),
-    ],
+    entry: './src/index.js',
     output: {
-        publicPath: '/',
-        path: path.join(__dirname, '/dev'),
-        filename: 'bundle.js'
+        path: '/build',
+        filename: '[name].[hash].js',
     },
     resolve: {
         extensions: ['.js', '.jsx', '.scss', '.css']
     },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: [
-                    'react-hot-loader',
-                    'babel-loader'
-                ]
-            },
-            {
-                test: /\.(jpg|jpeg|png|svg|gif|bmp)/i,
-                use: [
-                    'url-loader?limit=5000'
-                ]
-            },
-            {
-                test: /\.(png|woff|woff2|svg|ttf|eot)($|\?)/i,
-                use: [
-                    'url-loader?limit=5000'
-                ]
-            },
-            // css文件的处理
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: "css-loader"
-                })
-            },
-            {
-                test: /\.(scss)$/,
-                // exclude: /node_modules/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    //resolve-url-loader may be chained before sass-loader if necessary
-                    use: [{
-                            loader: "css-loader",
-                            options: {
-                                sourceMap: true
-                            }
-                        }, {
-                            loader: 'postcss-loader',
-                            options: {
-                                sourceMap: true
-                            }
-                        }, {
-                            loader: "sass-loader",
-                            options: {
-                                sourceMap: true
-                            }
-                        }]
-                })
-            }
+    module:{
+        rules:[{
+            test:/\.(js|jsx)$/,
+            include: /src/,
+            use: [{
+                loader:'babel-loader'
+            }]
+        }, {
+            test: /\.html$/,
+            use: [{
+                loader: "html-loader"
+            }]
+        }, {
+            test: /\.(sa|sc|c)ss$/,
+            // include: /src/,
+            use:[{
+                loader: MiniCssExtractPlugin.loader
+            },{
+                loader:'css-loader'
+            },{
+                loader:'postcss-loader'
+            },{
+                loader: "sass-loader"
+            }]
+        }, {
+            test: /\.(png|jpe?g|gif)$/,
+            use:[{
+                loader: 'url-loader?limit=10000&name=img/[name].[ext]'
+            }]
+        }
         ]
     },
+    optimization:{
+        // splitChunks: {
+        //     chunks: 'all',
+        //     // name: false
+        // }
+        runtimeChunk: 'single',
+        splitChunks: {
+            chunks: 'all',
+            maxInitialRequests: Infinity,
+            minSize: 1024 * 50,
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name(module) {
+                        // get the name. E.g. node_modules/packageName/not/this/part.js
+                        // or node_modules/packageName
+                        const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+                        // npm package names are URL-safe, but some servers don't like @ symbols
+                        return `npm.${packageName.replace('@', '')}`;
+                    },
+                },
+            },
+        }
+    },
     plugins: [
-        // Scope hosting
-        new webpack.optimize.ModuleConcatenationPlugin(),
-
         // 独立css文件
-        new ExtractTextPlugin({
-            filename: 'css/main.css',
-            disable: true
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
         }),
-
-        // 提出公共模块
-        new webpack.optimize.CommonsChunkPlugin({
-            name : 'common',
-            filename: 'js/base.js'
-        }),
-
         // html 模板插件
         new HtmlWebpackPlugin({
             template: __dirname + '/src/index.html'
-        }),
-        // 热加载插件
-        new webpack.HotModuleReplacementPlugin(),
-        // 可在业务 js 代码中使用 __DEV__ 判断是否是dev模式（dev模式下可以提示错误、测试报告等, production模式不提示）
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
         })
     ],
     devServer: {
@@ -114,9 +89,6 @@ module.exports = {
         //     }
         // },
         port: '9010',
-        // proxy: {
-        //     '/api':'http://admintest.happymmall.com',
-        // },
         proxy : {
             '/manage' : {
                 target: 'http://admintest.happymmall.com',
